0f49e1bc5a938d3285fdf68ebcdbb9af5749315d
vanshita80048 <vanshita479@gmail.com>
2023-05-21 15:58:25 +0530

    Operators and Expressions

---
+++ Operators.js
@@ -0,0 +1,57 @@
+console.log("Operators in Js")
+
+//Arithmetic Operators
+let a = 10;
+let b = 5;
+console.log("a + b = ",a + b)
+console.log("a - b = ",a - b)
+console.log("a / b = ",a / b)
+console.log("a ** b = ",a ** b)
+console.log("a % b = ",a % b)
+console.log("++a = ",++a)
+console.log("a++ = ",a++)
+console.log("--a = ",--a)
+console.log("a-- = ",a--)
+console.log("a = ",a)
+
+//Assignment Operators
+let assignment = 1;
+assignment += 5;
+//assignment = assignment + 5
+console.log("assignment = ",assignment);
+
+
+assignment -= 5
+//assignment = assignment - 5 
+console.log("assignment = ",assignment);
+
+assignment *= 5 
+//assignment = assignment * 5;
+console.log("assignment = ",assignment);
+
+assignment /= 5
+//assignment = assignment / 5;
+console.log("assignment = ",assignment);
+
+//Comparison Operators
+let var1 = 9;
+let var2 = 8;
+console.log("var1 == var2 is ",var1==var2)
+console.log("var1 != var2 is ",var1!=var2)
+console.log("var1 === var2 is ",var1 === var2)
+console.log("var1 !== var2 is ",var1 !== var2)
+console.log("var1 > var2 is ",var1 > var2)
+
+//Logical Operators
+let x = 7;
+let y = 6;
+console.log(x > y && x == 7)
+console.log(x < y || x == 7)
+console.log(!false)
+console.log(!true)
+
+//Comments
+//This is a single line comment
+/* This a multi - line 
+comment */
+
