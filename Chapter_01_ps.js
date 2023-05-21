81c1116ab9808257896e8c4cda6f664bbe261731
vanshita80048 <vanshita479@gmail.com>
2023-05-21 15:18:51 +0530

    Chapter 01 Practice set

---
+++ Chapter_01_ps.js
@@ -0,0 +1,32 @@
+//Q-01
+let a = "I am a girl";
+let b = 2;
+console.log(a + b);
+
+//Q-02
+console.log(typeof (a + b));
+
+//Q-03
+const c = {
+    name: "vanshita",
+    profession: "student",
+    Roll_number: 57 
+}
+
+// c = 67; // It shows a type error because c is a constant and it can't be changed.
+
+//Q-04
+//Here we have to write 'friend' in a string form otherwise it will show an error to our code and tell that friend is not defined.
+c ['friend'] ="Janvi"
+console.log(c)
+
+//Q-05
+
+const dict = {
+    Quixotic : "unrealistic",
+    Mellifluous : "sweet sound",
+    Ephermal : "short-lived",
+    Nebulous : "unclear" 
+}
+
+console.log(dict);
