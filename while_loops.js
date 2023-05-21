01109747eacda46312df3230486814e9c3e5d5f4
vanshita80048 <vanshita479@gmail.com>
2023-05-21 20:01:46 +0530

    While and do-while loop

---
+++ while_loops.js
@@ -0,0 +1,16 @@
+//While loop
+let n = prompt("Enter a number")
+n = Number.parseInt(n)
+
+let i = 0;
+while(i<n){
+    console.log(i)
+    i++;
+}
+
+// do while loop
+let p = 10;
+do{
+    console.log(p)
+    p++;
+}while(p < n)
