2508c9b5084ebf868f1fb418289bf930b8023ecf
vanshita80048 <vanshita479@gmail.com>
2023-05-22 20:15:24 +0530

    Strings

---
+++ strings.js
@@ -0,0 +1,17 @@
+let name = "Vanshita"
+
+console.log(name);
+console.log(name.length);
+console.log(name[0] + name[1] + name[2] + name[3]+ name[4]);
+
+//Tempelate literals
+let girl1 = "Sakshi"
+let girl2 = "Anjali"
+
+//Anjali is the sister of Sakshi
+console.log(`${girl2} is the sister of ${girl1}`)
+
+//If you want to add double quote/single quote character in the string then use escape sequence characters
+
+let name1 = "Avi\'nash"
+console.log(name1)
