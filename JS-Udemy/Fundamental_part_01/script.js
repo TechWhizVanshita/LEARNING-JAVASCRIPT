a635a7c67c0a0604919b985f09fe700efe2d4638
Vanshita <120383319+vanshita-80048@users.noreply.github.com>
2023-09-28 16:14:12 +0530

    commit

---
+++ JS-Udemy/Fundamental_part_01/script.js
@@ -0,0 +1,15 @@
+let js = 'amazing';
+if(js=== 'amazing')
+  alert("js is fun")
+
+console.log(50+20)
+
+//assignment - values and variables
+
+let country = "India";
+let continent = "Asia";
+let population = "361 million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
