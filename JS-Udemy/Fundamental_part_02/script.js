e6d948f926e2a0a4369d8745b41f308b1e023661
vanshita80048 <vanshita479@gmail.com>
2023-10-18 18:53:15 +0530

    Function complete

--- JS-Udemy/Fundamental_part_02/script.js
+++ JS-Udemy/Fundamental_part_02/script.js
@@ -13,3 +13,20 @@ function fruitProcessor(apples, oranges){
 }
 
 fruitProcessor(5, 4);
+
+//Arrow function
+
+const calcAge = (birthYear) => 2033-birthYear;
+const age = calcAge(2005);
+
+console.log(age);
+
+const yearUntilRetirement = (birthYear,firstName) => {
+    const age1 = 2047 - birthYear;
+    const retirement = 65 - age1;
+
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearUntilRetirement(1991,'Jonas'));
+console.log(yearUntilRetirement(1980,'Bob'));
