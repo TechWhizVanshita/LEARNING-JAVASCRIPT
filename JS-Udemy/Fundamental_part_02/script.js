667a36c58e04561f7939d8bb8f765922f133f476
vanshita80048 <vanshita479@gmail.com>
2023-10-19 00:38:44 +0530

    commit

--- JS-Udemy/Fundamental_part_02/script.js
+++ JS-Udemy/Fundamental_part_02/script.js
@@ -14,8 +14,8 @@ function fruitProcessor(apples, oranges){
 
 fruitProcessor(5, 4);
 
-//Arrow function
 
+//Arrow function
 const calcAge = (birthYear) => 2033-birthYear;
 const age = calcAge(2005);
 
@@ -30,3 +30,87 @@ const yearUntilRetirement = (birthYear,firstName) => {
 
 console.log(yearUntilRetirement(1991,'Jonas'));
 console.log(yearUntilRetirement(1980,'Bob'));
+
+//Function calling other functions
+
+const cutPieces = function(fruit){
+    return fruit * 4;
+}
+
+const fruitProcessor = function(apples,oranges){
+
+    const applePieces = cutPieces(apples);
+    const orangePieces = cutPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+
+    return juice;
+}
+
+
+console.log(fruitProcessor(2,3))
+
+
+//Reviewing functions
+
+const calcAge3 = function(birthYear){
+    return 2047-birthYear;
+}
+
+const yearUntilRetirement1 = function(birthYear,firstName){
+    const age2 = calcAge3(birthYear);
+    const retirement = 65-age2;
+
+    if(retirement>0){
+        console.log(`${firstName} retires in ${retirement} years.`);
+        return retirement;
+    }
+
+    else{
+       console.log(`${firstName} has already retired.`);
+    }
+}
+
+console.log(yearUntilRetirement1(2005,'Vanshita'));
+
+//Array methods
+
+
+//Add elements
+const persons = ['Sita','Ram','Kokila','Dashrath','Parth'];
+const newPerson = persons.push('Siya');
+
+console.log(persons);
+console.log(newPerson); //Returns length of new array
+
+persons.unshift('Vansh');//adds at the starting of the array
+console.log(persons);
+
+//Remove elements
+
+persons.pop(); //Removes last element from the array
+
+const popped = persons.pop();
+console.log(popped);
+console.log(persons);
+
+persons.shift(); //Removes first element from the array
+console.log(persons);
+
+//indexOf() -> returns the index number of the array
+
+console.log(persons.indexOf('Kokila')); //return index number 2
+
+//includes() -> returns boolean value = true/false
+
+console.log(persons.includes('Ram'));
+console.log(persons.includes('Dashrath'));
+console.log(persons.includes('Damini'));
+
+//objects(key-value pairs)
+
+
+
+
+
+
