d749d39078cf91054cdd4839ec741de391fce3b8
vanshita80048 <vanshita479@gmail.com>
2023-10-19 18:43:05 +0530

    commit

--- JS-Udemy/Fundamental_part_02/script.js
+++ JS-Udemy/Fundamental_part_02/script.js
@@ -1,13 +1,13 @@
 'use strict';
 
-function logger(){
+function logger() {
     console.log('My name is Vanshita');
 }
 
 logger();
 
-function fruitProcessor(apples, oranges){
-    console.log(apples,oranges);
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
     const juice = `Juice with ${apples} and ${oranges} oranges.`
     return juice;
 }
@@ -16,28 +16,28 @@ fruitProcessor(5, 4);
 
 
 //Arrow function
-const calcAge = (birthYear) => 2033-birthYear;
+const calcAge = (birthYear) => 2033 - birthYear;
 const age = calcAge(2005);
 
 console.log(age);
 
-const yearUntilRetirement = (birthYear,firstName) => {
+const yearUntilRetirement = (birthYear, firstName) => {
     const age1 = 2047 - birthYear;
     const retirement = 65 - age1;
 
     return `${firstName} retires in ${retirement} years`;
 }
 
-console.log(yearUntilRetirement(1991,'Jonas'));
-console.log(yearUntilRetirement(1980,'Bob'));
+console.log(yearUntilRetirement(1991, 'Jonas'));
+console.log(yearUntilRetirement(1980, 'Bob'));
 
 //Function calling other functions
 
-const cutPieces = function(fruit){
+const cutPieces = function (fruit) {
     return fruit * 4;
 }
 
-const fruitProcessor = function(apples,oranges){
+const fruitProcessor = function (apples, oranges) {
 
     const applePieces = cutPieces(apples);
     const orangePieces = cutPieces(oranges);
@@ -48,42 +48,40 @@ const fruitProcessor = function(apples,oranges){
 }
 
 
-console.log(fruitProcessor(2,3))
+console.log(fruitProcessor(2, 3))
 
 
 //Reviewing functions
 
-const calcAge3 = function(birthYear){
-    return 2047-birthYear;
+const calcAge3 = function (birthYear) {
+    return 2047 - birthYear;
 }
 
-const yearUntilRetirement1 = function(birthYear,firstName){
+const yearUntilRetirement1 = function (birthYear, firstName) {
     const age2 = calcAge3(birthYear);
-    const retirement = 65-age2;
+    const retirement = 65 - age2;
 
-    if(retirement>0){
+    if (retirement > 0) {
         console.log(`${firstName} retires in ${retirement} years.`);
         return retirement;
-    }
-
-    else{
-       console.log(`${firstName} has already retired.`);
+    } else {
+        console.log(`${firstName} has already retired.`);
     }
 }
 
-console.log(yearUntilRetirement1(2005,'Vanshita'));
+console.log(yearUntilRetirement1(2005, 'Vanshita'));
 
 //Array methods
 
 
 //Add elements
-const persons = ['Sita','Ram','Kokila','Dashrath','Parth'];
+const persons = ['Sita', 'Ram', 'Kokila', 'Dashrath', 'Parth'];
 const newPerson = persons.push('Siya');
 
 console.log(persons);
 console.log(newPerson); //Returns length of new array
 
-persons.unshift('Vansh');//adds at the starting of the array
+persons.unshift('Vansh'); //adds at the starting of the array
 console.log(persons);
 
 //Remove elements
@@ -112,9 +110,9 @@ console.log(persons.includes('Damini'));
 const virat = {
     firstName: 'Virat',
     lastName: 'Kohli',
-    age: 2047-1992,
+    age: 2047 - 1992,
     job: 'Cricketer',
-    friends: ['Anushka','Shikhar','Soumya']
+    friends: ['Anushka', 'Shikhar', 'Soumya']
 };
 
 console.log(virat);
@@ -149,12 +147,12 @@ console.log(`${virat.firstName} has ${virat.friends.length} friends, and his bes
 
 //Object methods
 
-const uncle ={
+const uncle = {
     firstName: 'Uncle',
     lastName: 'John',
     birthYear: 1957,
-    calcAge: function(){
-        return 2023-this.birthYear;
+    calcAge: function () {
+        return 2023 - this.birthYear;
     },
 };
 
@@ -167,14 +165,14 @@ console.log(uncle['calcAge'](this.birthYear));
 const rohit = [
     'Rohit',
     'Sharma',
-    2047-1991,
+    2047 - 1991,
     'Bowler',
-    ['Hardik','Ishan','Rahul']
+    ['Hardik', 'Ishan', 'Rahul']
 ];
 
 const types = [];
 
-for(let i = 0; i<=(rohit.length-1); i++){
+for (let i = 0; i <= (rohit.length - 1); i++) {
 
     //reading from rohit's array
     console.log(rohit[i]);
@@ -189,7 +187,7 @@ console.log(types);
 
 let dice = Math.trunc(Math.random() * 6) + 1;
 
-while(dice!==6){
+while (dice !== 6) {
     console.log(`You rolled a ${dice}`);
     dice = Math.trunc(Math.random() * 6) + 1;
 }
